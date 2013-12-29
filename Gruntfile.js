6a219632eefe5d2c2cf0ee730e6af1a4bfe966d5
Leny <info@flatland.be>
2013-12-29 18:51:01 +0100

    Removing unused default unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,41 +16,38 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
       ],
       options: {
         jshintrc: '.jshintrc',
       },
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
+    // Configuration to be run.
     todo: {
       default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+        options: {},
+        src: [
+          'test/*'
+        ],
       },
       custom_options: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!',
+          marks: [
+            {
+              pattern: "BURP",
+              color: "pink"
+            },
+            {
+              name: "TODO",
+              pattern: /TODO/,
+              color: "yellow"
+            }
+          ]
         },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+        src: [
+          'test/*'
+        ],
       },
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
     },
 
   });
@@ -60,14 +57,8 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'todo', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  // By default, lint and run todo.
+  grunt.registerTask('default', ['jshint', 'todo']);
 
 };
